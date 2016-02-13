5ce30e2709f9cd370cd8890071e2c1a41190f7ec
peter <spr.perfect@gmail.com>
2016-02-13 22:34:00 +0800

    init commit

---
+++ Java.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1455373239233" ID="ID_1786357460" MODIFIED="1455373255595" TEXT="Java">
+<node CREATED="1455373257891" ID="ID_1866614440" MODIFIED="1455373283348" POSITION="right" TEXT="Language">
+<node CREATED="1455373284830" ID="ID_1234235553" MODIFIED="1455373292903" TEXT="Class and Object"/>
+</node>
+</node>
+</map>
